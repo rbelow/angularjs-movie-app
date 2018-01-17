1a175abb7bc6a00844fd25fa2cdddc5101f2b635
Ruben Below <rbelow@hagafu.com>
2018-01-17 21:24:57 +0100

    added angular resource to index.html

--- src/movie-core/service.js
+++ src/movie-core/service.js
@@ -1,6 +1,9 @@
 angular // eslint-disable-line
   .module('movieCore', ['ngResource'])
   .factory('PopularMovies', function ($resource) {
+    // a factory which creates a resource object that lets you interact with restful server-side data sources
+    // requires the `ngResource` module to be installed
+    // https://docs.angularjs.org/api/ngResource/service/$resource
     var token = 'teddybear' // TBC
     return $resource('popular/:movieId', { movieId: '@id' }, {
       update: {
